af5edde3c11562f2cb7fea1b69687193fa52a8b9
Daniel Wagner <daniel.wagner@1und1.de>
2012-12-20 16:29:09 +0100

    support for qx.ui.tree.VirtualTree

---
+++ src/org/oneandone/qxwebdriver/ui/tree/VirtualTree.java
@@ -0,0 +1,13 @@
+package org.oneandone.qxwebdriver.ui.tree;
+
+import org.oneandone.qxwebdriver.QxWebDriver;
+import org.oneandone.qxwebdriver.ui.list.List;
+import org.openqa.selenium.WebElement;
+
+public class VirtualTree extends List {
+
+	public VirtualTree(WebElement element, QxWebDriver webDriver) {
+		super(element, webDriver);
+	}
+
+}
