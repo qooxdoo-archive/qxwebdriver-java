06fae43a0543dc3960e2bc76bfdcb20211b5254a
Daniel Wagner <daniel.wagner@1und1.de>
2013-01-11 17:52:25 +0100

    converted examples to JUnit

--- src/main/java/org/oneandone/qxwebdriver/examples/widgetbrowser/Common.java
+++ src/main/java/org/oneandone/qxwebdriver/examples/widgetbrowser/Common.java
@@ -1,27 +1,37 @@
 package org.oneandone.qxwebdriver.examples.widgetbrowser;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.oneandone.qxwebdriver.By;
 import org.oneandone.qxwebdriver.QxWebDriver;
 import org.oneandone.qxwebdriver.ui.Selectable;
 import org.oneandone.qxwebdriver.ui.Widget;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-/**
- * Common base class for Widget Browser tests
- *
- */
 public class Common {
-	
-	protected QxWebDriver driver;
-	protected Widget tabPage;
-	
-	public Common(QxWebDriver qxwebdriver) {
-		driver = qxwebdriver;
+
+	static QxWebDriver driver;
+	static final String AUT_URL = "http://demo.qooxdoo.org/current/widgetbrowser/index.html";
+
+	static Widget tabPage;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		FirefoxDriver webDriver = new FirefoxDriver();
+		//ChromeDriver webDriver = new ChromeDriver();
+		driver = new QxWebDriver(webDriver);
+		driver.get(AUT_URL);
 	}
-	
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		driver.close();
+	}
+
 	/**
 	 * Clicks a button in the Widget Browser's main tab bar
 	 */
-	public void selectTab(String title) {
+	public static void selectTab(String title) {
 		String locator = "qx.ui.container.Composite/qx.ui.container.Scroll/qx.ui.tabview.TabView";
 		Selectable tabView = (Selectable) driver.findWidget(By.qxh(locator));
 		tabView.selectItem(title);
@@ -29,9 +39,4 @@ public class Common {
 		String tabPageLocator = "qx.ui.tabview.Page";
 		tabPage = tabView.findWidget(By.qxh(tabPageLocator));
 	}
-	
-	public void test() {
-		System.out.println("Nothing to do.");
-	}
-
 }
---
+++ src/main/java/org/oneandone/qxwebdriver/examples/widgetbrowser/TestToolbarMenu.java
@@ -0,0 +1,50 @@
+package org.oneandone.qxwebdriver.examples.widgetbrowser;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.oneandone.qxwebdriver.By;
+import org.oneandone.qxwebdriver.ui.Selectable;
+import org.oneandone.qxwebdriver.ui.Widget;
+
+public class TestToolbarMenu extends Common {
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Common.setUpBeforeClass();
+		selectTab("Toolbar.*");
+	}
+	
+	@Test
+	public void menu() {
+		By menuLocator = By.qxh("*/qx.ui.menu.Menu");
+		Selectable menu = (Selectable) tabPage.findWidget(menuLocator);
+		Widget menuRadioButton = menu.getSelectableItem("Menu RadioButton");
+		boolean selectedBefore = (Boolean) menuRadioButton.getPropertyValue("value");
+		menu.selectItem("Menu RadioButton");
+		boolean selectedAfter = (Boolean) menuRadioButton.getPropertyValue("value");
+		assertFalse(selectedBefore == selectedAfter);
+		
+		By splitButtonLocator = By.qxh("*/qx.ui.toolbar.SplitButton");
+		Widget tbarSplit = tabPage.findWidget(splitButtonLocator);
+		tbarSplit.getChildControl("arrow").click();
+		Selectable splitMenu = (Selectable) tbarSplit.getWidgetFromProperty("menu");
+		splitMenu.selectItem(0);
+		
+		By menuButtonLocator = By.qxh("*/[@label=MenuButton]");
+		Selectable tbarMenuButton = (Selectable) tabPage.findWidget(menuButtonLocator);
+		
+		// Must click the button so the menu is rendered before we can check if
+		// the MenuRadioButton is selected
+		tbarMenuButton.click();
+		Selectable buttonMenu = (Selectable) tbarMenuButton.getWidgetFromProperty("menu");
+		Widget buttonMenuRadioButton = buttonMenu.getSelectableItem("Menu RadioButton");
+		selectedBefore = (Boolean) buttonMenuRadioButton.getPropertyValue("value");
+		tbarMenuButton.click();
+		tbarMenuButton.selectItem("Menu RadioButton");
+		selectedAfter = (Boolean) buttonMenuRadioButton.getPropertyValue("value");
+		assertFalse(selectedBefore == selectedAfter);
+	}
+
+}
