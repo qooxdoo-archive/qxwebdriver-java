a52ff93d63c768d95b52d55169c67c62a63f5561
Daniel Wagner <daniel.wagner@1und1.de>
2013-01-11 17:48:49 +0100

    better error message

--- src/main/resources/javascript/getElementFromProperty.js
+++ src/main/resources/javascript/getElementFromProperty.js
@@ -1,5 +1,10 @@
 var getElementFromProperty = function() {
   var widget = qx.ui.core.Widget.getWidgetByElement(arguments[0]);
   var propVal = widget.get(arguments[1]);
-  return propVal.getContentElement().getDomElement();
+
+  try {
+    return propVal.getContentElement().getDomElement();
+  } catch(ex) {
+    throw new Error("Couldn't get DOM element from widget " + propVal.toString() + ": " + ex.message);
+  }
 };
