bf3a1cb884dc6e979126730a3f2b6d9d8bec7ba0
Daniel Wagner <daniel.wagner@1und1.de>
2013-01-16 10:56:40 +0100

    added copyright header

--- src/main/java/org/oneandone/qxwebdriver/QxWebDriver.java
+++ src/main/java/org/oneandone/qxwebdriver/QxWebDriver.java
@@ -1,3 +1,22 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2012-2013 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Daniel Wagner (danielwagner)
+
+************************************************************************ */
+
 package org.oneandone.qxwebdriver;
 
 import java.util.List;
@@ -34,8 +53,7 @@ public class QxWebDriver implements WebDriver {
 	
 	public QxWebDriver(WebDriver webdriver, WidgetFactory widgetFactory) {
 		driver = webdriver;
-		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-		jsRunner = new JavaScriptRunner(jsExecutor);
+		jsExecutor = (JavascriptExecutor) driver;
 		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
 	}
 	
