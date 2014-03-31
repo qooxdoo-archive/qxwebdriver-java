901e5da598ad32c612e21ae11b88e389d25a893c
danielwagner <daniel.wagner@1und1.de>
2014-03-31 13:50:55 +0200

    Data Binding test case

---
+++ src/test/java/org/qooxdoo/demo/mobileshowcase/DataBinding.java
@@ -0,0 +1,45 @@
+package org.qooxdoo.demo.mobileshowcase;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.oneandone.qxwebdriver.By;
+import org.oneandone.qxwebdriver.ui.mobile.core.WidgetImpl;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.HasTouchScreen;
+
+public class DataBinding extends Mobileshowcase {
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Mobileshowcase.setUpBeforeClass();
+		scrollTo(0, 5000);
+		Thread.sleep(500);
+		selectItem("Data Binding");
+		verifyTitle("Data Binding");
+	}
+	
+	@Test
+	public void slider() throws InterruptedException {
+		if (!(driver.getWebDriver() instanceof HasTouchScreen)) {
+			return;
+		}
+		WidgetImpl input = (WidgetImpl) driver.findWidget(By.xpath("//input"));
+		int valueBefore = Integer.parseInt((String) input.getPropertyValue("value"));
+		
+		WidgetImpl slider = (WidgetImpl) driver.findWidget(By.xpath("//div[contains(@class, 'slider')]"));
+		slider.track(200, 0, 10);
+		
+		int valueAfter = Integer.parseInt((String) input.getPropertyValue("value"));
+		Assert.assertTrue(valueAfter > valueBefore);
+	}
+	
+	@Test
+	public void time() throws InterruptedException {
+		WebElement button = driver.findElement(By.xpath("//div[text() = 'Take Time Snapshot']/ancestor::div[contains(@class, 'button')]"));
+		tap(button);
+		WebElement entry = driver.findElement(By.xpath("//div[text() = 'Stop #1']"));
+		Assert.assertTrue(entry.isDisplayed());
+	}
+
+}
