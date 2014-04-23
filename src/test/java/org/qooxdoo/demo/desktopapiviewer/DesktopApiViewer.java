028e03f596e947a2dd91dc489a8a3e8ed33fe8f8
danielwagner <daniel.wagner@1und1.de>
2014-04-23 17:25:56 +0200

    qx.Desktop API Viewer test case

---
+++ src/test/java/org/qooxdoo/demo/desktopapiviewer/DesktopApiViewer.java
@@ -0,0 +1,202 @@
+package org.qooxdoo.demo.desktopapiviewer;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.oneandone.qxwebdriver.By;
+import org.oneandone.qxwebdriver.ui.Selectable;
+import org.oneandone.qxwebdriver.ui.Widget;
+import org.oneandone.qxwebdriver.ui.table.Table;
+import org.oneandone.qxwebdriver.ui.tree.core.AbstractItem;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.qooxdoo.demo.IntegrationTest;
+
+public class DesktopApiViewer extends IntegrationTest {
+	
+	protected String tabButtonPath = "*/apiviewer.DetailFrameTabView/*/qx.ui.tabview.TabButton";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		IntegrationTest.setUpBeforeClass();
+		driver.jsExecutor.executeScript("qx.locale.Manager.getInstance().setLocale('en');");
+	}
+	
+	protected void selectView(String label) {
+		String path = "*/qx.ui.toolbar.ToolBar/*/[@label=" + label + "]";
+		Widget button = driver.findWidget(By.qxh(path));
+		boolean selected = (Boolean) button.getPropertyValue("value");
+		if (!selected) {
+			button.click();
+			selected = (Boolean) button.getPropertyValue("value");
+			Assert.assertTrue(selected);
+		}
+	}
+
+	@Test
+	public void tree() {
+		selectView("Content");
+		
+		Selectable tree = (Selectable) driver.findWidget(By.qxh("*/apiviewer.ui.PackageTree"));
+		Assert.assertTrue(tree.isDisplayed());
+		
+		String item1Label = "bom";
+		AbstractItem item1 = (AbstractItem) tree.getSelectableItem(item1Label);
+		item1.click();
+		Assert.assertTrue(item1.isOpen());
+		
+		String item2Label = "element";
+		AbstractItem item2 = (AbstractItem) tree.getSelectableItem(item2Label);
+		item2.click();
+		Assert.assertTrue(item2.isOpen());
+
+		String item3Label = "Dimension";
+		AbstractItem item3 = (AbstractItem) tree.getSelectableItem(item3Label);
+		item3.click();
+		
+		Widget tabButton = driver.findWidget(By.qxh(tabButtonPath));
+		Assert.assertEquals("qx.bom.element.Dimension", tabButton.getPropertyValue("label"));
+		
+		String hash = (String) driver.executeScript("return location.hash");
+		Assert.assertEquals("#qx.bom.element.Dimension", hash);
+		
+	}
+	
+	@Test
+	public void search() {
+		selectView("Search");
+		
+		String tablePath = "*/apiviewer.ui.SearchView/*/qx.ui.table.Table";
+		Table table = (Table) driver.findWidget(By.qxh(tablePath));
+		Long initialRowCount = (Long) table.getRowCount();
+		Assert.assertEquals(Double.valueOf(0), Double.valueOf(initialRowCount));
+		
+		String searchFieldPath = "*/apiviewer.ui.SearchView/*/qx.ui.form.TextField";
+		Widget searchField = driver.findWidget(By.qxh(searchFieldPath));
+		searchField.sendKeys("window");
+		Long resultRowCount = (Long) table.getRowCount();
+		Assert.assertTrue(resultRowCount > 0);
+		
+		String namespaceFieldPath = "*/apiviewer.ui.SearchView/qx.ui.container.Composite/child[3]";
+		Widget namespaceField = driver.findWidget(By.qxh(namespaceFieldPath));
+		namespaceField.sendKeys("window");
+		Long filteredRowCount = (Long) table.getRowCount();
+		Assert.assertTrue(filteredRowCount < resultRowCount);
+		
+		Widget toggleButton = driver.findWidget(By.qxh("*/apiviewer.ui.SearchView/*/[@label=Toggle Filters]"));
+		toggleButton.click();
+		Long toggledCount = (Long) table.getRowCount();
+		Assert.assertEquals(Double.valueOf(0), Double.valueOf(toggledCount));
+		
+		String[] itemIcons = {"package","class","method_public", "constant", "property", "event", "childcontrol"};
+		for (String icon: itemIcons) {
+			String buttonPath = "*/apiviewer.ui.SearchView/*/[@icon=" + icon + "]";
+			Widget button = driver.findWidget(By.qxh(buttonPath));
+			button.click();
+			if (!icon.equals("event") && !icon.equals("childcontrol")) {
+				Long newCount = (Long) table.getRowCount();
+				Assert.assertTrue(newCount > toggledCount);
+				toggledCount = newCount;
+			}
+		}
+	}
+	
+	protected void loadFromHash(String className) {
+		String url = driver.getCurrentUrl();
+		String[] parts = url.split("#");
+		url = parts[0] + "#" + className;
+		driver.get(url);
+		driver.registerLogAppender();
+		driver.registerGlobalErrorHandler();
+		Widget tabButton = driver.findWidget(By.qxh(tabButtonPath));
+		System.out.println("getPropertyValue");
+		Assert.assertEquals(className, tabButton.getPropertyValue("label"));
+	}
+	
+	protected void testProperties() {
+		String propertiesPath = "*/qx.ui.toolbar.ToolBar/*/[@label=Properties]";
+		Widget propertiesButton = driver.findWidget(By.qxh(propertiesPath));
+		boolean propertiesActive = (Boolean) propertiesButton.getPropertyValue("value"); 
+		Assert.assertTrue(propertiesActive);
+		String propertyItemPath = "//div[contains(@class, 'info-panel')]/descendant::span[text()='getLiveResize']";
+		WebElement propertyItem = driver.findElement(By.xpath(propertyItemPath));
+		Assert.assertTrue(propertyItem.isDisplayed());
+		propertiesButton.click();
+		driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+		try {
+			propertyItem = driver.findElement(By.xpath(propertyItemPath));
+			Assert.assertTrue("Property method was not hidden!", false);
+		} catch(NoSuchElementException e) {}
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+		propertiesButton.click();
+	}
+	
+	protected void testClassItem(String item, String method) {
+		String itemPath = "*/qx.ui.toolbar.ToolBar/*/[@label=" + item + "]";
+		Widget itemButton = driver.findWidget(By.qxh(itemPath));
+		boolean itemActive = (Boolean) itemButton.getPropertyValue("value"); 
+		Assert.assertFalse(itemActive);
+		String methodPath = "//div[contains(@class, 'info-panel')]/descendant::span[text()='" + method + "']";
+		driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+		try {
+			driver.findElement(By.xpath(methodPath));
+			Assert.assertTrue(item + " item " + method + " was not hidden initially!", false);
+		} catch(NoSuchElementException e) {}
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+		itemButton.click();
+		
+		WebElement methodItem = driver.findElement(By.xpath(methodPath));
+		Assert.assertTrue(methodItem.isDisplayed());
+		itemButton.click();
+		
+		driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+		try {
+			driver.findElement(By.xpath(methodPath));
+			Assert.assertTrue(item + " item " + method + " was not hidden!", false);
+		} catch(NoSuchElementException e) {}
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+	}
+	
+	protected void testIncludes() {
+		String inheritedItemPath = "//div[contains(@class, 'info-panel')]/descendant::span[text()='changeTextColor']";
+		
+		driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+		try {
+			driver.findElement(By.xpath(inheritedItemPath));
+			Assert.assertTrue("Inherited method was not hidden!", false);
+		} catch(NoSuchElementException e) {}
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+		
+		String includesPath = "*/qx.ui.toolbar.ToolBar/*/[@label=Includes]";
+		Selectable includesButton = (Selectable) driver.findWidget(By.qxh(includesPath));
+		includesButton.selectItem("Inherited");		
+		WebElement inheritedItem = driver.findElement(By.xpath(inheritedItemPath));
+		Assert.assertTrue(inheritedItem.isDisplayed());
+
+		includesButton.selectItem("Inherited");
+		driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+		try {
+			driver.findElement(By.xpath(inheritedItemPath));
+			Assert.assertTrue("Inherited method was not hidden!", false);
+		} catch(NoSuchElementException e) {}
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void classItems() {
+		String className = "qx.ui.table.pane.Scroller";
+		loadFromHash(className);
+		
+		testProperties();
+		testIncludes();
+		testClassItem("Protected", "_hideResizeLine");
+		testClassItem("Private", "__isAtEdge");
+		testClassItem("Internal", "getVerticalScrollBarWidth");
+	}
+	
+	//Links: Within class; to other classes
+	//Detail toggle
+	//Tabs
+}
