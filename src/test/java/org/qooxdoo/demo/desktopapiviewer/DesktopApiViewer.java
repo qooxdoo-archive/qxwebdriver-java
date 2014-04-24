31c183c093ab11fb55f2e295105e7385ae049081
danielwagner <daniel.wagner@1und1.de>
2014-04-24 17:53:52 +0200

    test tabs

--- src/test/java/org/qooxdoo/demo/desktopapiviewer/DesktopApiViewer.java
+++ src/test/java/org/qooxdoo/demo/desktopapiviewer/DesktopApiViewer.java
@@ -10,8 +10,10 @@ import org.oneandone.qxwebdriver.ui.Selectable;
 import org.oneandone.qxwebdriver.ui.Widget;
 import org.oneandone.qxwebdriver.ui.table.Table;
 import org.oneandone.qxwebdriver.ui.tree.core.AbstractItem;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.qooxdoo.demo.IntegrationTest;
 
 public class DesktopApiViewer extends IntegrationTest {
@@ -223,7 +225,7 @@ public class DesktopApiViewer extends IntegrationTest {
 		
 		String detailHeadlinePath = "//div[contains(@class, 'info-panel')]/descendant::div[contains(@class, 'item-detail-headline')]";
 		try {
-			WebElement detailHeadline = driver.findElement(By.xpath(detailHeadlinePath));
+			driver.findElement(By.xpath(detailHeadlinePath));
 			Assert.assertTrue("Constructor details should be hidden initially!", false);
 		} catch(NoSuchElementException e) {}
 		
@@ -238,5 +240,31 @@ public class DesktopApiViewer extends IntegrationTest {
 			Assert.assertTrue("Constructor details could not be hidden!", false);
 		} catch(NoSuchElementException e) {}
 	}
-	//Tabs
+	
+	@Test
+	public void tabs() {
+		loadFromHash("qx.ui.form.Button");
+		
+		String newTabClass = "qx.ui.form.MenuButton";
+		WebElement link = driver.findElement(By.xpath("//a[text()='" + newTabClass + "']"));
+		Actions action = new Actions(driver.getWebDriver());
+		action.keyDown(Keys.SHIFT);
+		action.click(link);
+		action.keyUp(Keys.SHIFT);
+		action.perform();
+		
+		String newTabPath = "*/apiviewer.DetailFrameTabView/*/[@label=" + newTabClass + "]";
+		Widget newTabButton = driver.findWidget(By.qxh(newTabPath));
+		Assert.assertTrue(newTabButton.isDisplayed());
+		
+		String closeButtonPath = newTabPath + "/qx.ui.form.Button";
+		Widget closeButton = driver.findWidget(By.qxh(closeButtonPath));
+		Assert.assertTrue(closeButton.isDisplayed());
+		closeButton.click();
+		
+		try {
+			driver.findWidget(By.qxh(newTabPath));
+			Assert.assertTrue("New tab was not closed!", false);
+		} catch(NoSuchElementException e) {}
+	}
 }
